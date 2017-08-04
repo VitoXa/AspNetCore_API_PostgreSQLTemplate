584b47d2a26f9118efb6292b10cd909bdfd071a9
VitoXa <tuvit17@yandex.ru>
2017-08-04 20:15:49 +0300

    Added simple model

---
+++ AspNetCore_API_PostgreSQLTemplate/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCore_API_PostgreSQLTemplate.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+
+}
---
+++ AspNetCore_API_PostgreSQLTemplate/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCore_API_PostgreSQLTemplate.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+    }
+}
